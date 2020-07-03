13c1ac82b267d7b5d219d7fb5b441eaefc860fb9
Advay Pal <advaypal@hotmail.com>
2020-07-03 16:01:20 -0700

    Finish vector functions

--- src/vector.rs
+++ src/vector.rs
@@ -1,10 +1,12 @@
-use core::ops::Neg;
-use core::ops::Mul;
-use core::ops::Add;
-use core::ops::Sub;
-use core::ops::MulAssign;
-use core::ops::DivAssign;
-use core::ops::AddAssign;
+use std::ops::Neg;
+use std::ops::Mul;
+use std::ops::Add;
+use std::ops::Sub;
+use std::ops::Div;
+use std::ops::MulAssign;
+use std::ops::DivAssign;
+use std::ops::AddAssign;
+use std::assert;
 
 #[derive(Copy, Clone)]
 enum VectorType {
@@ -93,17 +95,30 @@ impl Sub for Vector {
     }
 }
 
-impl Mul for Vector {
+impl Mul<f64> for Vector {
     type Output = Self;
 
-    fn mul(self, other: Self) -> Self {
+    fn mul(self, term: f64) -> Self {
         Self {
             vec_type: self.vec_type,
-            x: self.x * other.x,
-            y: self.y * other.y,
-            z: self.z * other.z,
+            x: self.x * term,
+            y: self.y * term,
+            z: self.z * term,
         }
-    }
+    }       
+}
+
+impl Div<f64> for Vector {
+    type Output = Self;
+
+    fn div(self, term: f64) -> Self {
+        Self {
+            vec_type: self.vec_type,
+            x: self.x / term,
+            y: self.y / term,
+            z: self.z / term,
+        }
+    }       
 }
 
 impl Vector {
@@ -113,5 +128,32 @@ impl Vector {
 
     fn length(&self) -> f64 {
         self.length_squared().sqrt()
+    }
+
+    fn dot(&self, other: Self) -> f64 {
+       self.x * other.x + self.y + other.y + self.z * other.z
+    }
+
+    fn cross(&self, other: Self) -> Self {
+        Self {
+            vec_type: self.vec_type,
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+
+    fn unit_vector(self) -> Self {
+        let length = self.length();
+        Self {
+            vec_type: self.vec_type,
+            x: self.x / length,
+            y: self.y / length,
+            z: self.z / length,           
+        }
+    }
+
+    fn print_vector(&self) -> () {
+        println!("{}, {}, {}\n", self.x, self.y, self.z);
     }
 }
