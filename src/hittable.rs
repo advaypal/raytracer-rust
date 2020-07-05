c0791ac771eea0d02c6e8a2dfe98470f7087ae58
Kwan Noel <noelkwan1998@gmail.com>
2020-07-05 22:59:33 +0800

    Finish 6.4

--- src/hittable.rs
+++ src/hittable.rs
@@ -9,6 +9,27 @@ pub struct HitRecord {
     pub p: Point,
     pub normal: Vector,
     pub t: f64,
+    pub front_face: RayLocale
+}
+
+// Whether ray is coming from inside or outside an object
+pub enum RayLocale {
+    Outside,
+    Inside
+}
+
+pub fn update_record_face(ray: &Ray, outward_normal: Vector) -> RayLocale {
+    match ray.direction.dot(outward_normal) < 0.0 {
+        true => RayLocale::Outside,
+        false => RayLocale::Inside,
+    }
+}
+
+pub fn update_record_normal(front_face: &RayLocale, outward_normal: Vector) -> Vector {
+    match front_face {
+        RayLocale::Outside => outward_normal,
+        RayLocale::Inside => -outward_normal,
+    }
 }
 
 pub trait Hittable {
